acacac323f180edf2116e89685f2614db512ccd8
R.Kawasaki <gettodowithallhaste9@gmail.com>
2020-04-24 16:52:39 +0000

    Fix user edit, Fix text color of follower list, Fix color of favorite button

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
                     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
                     uniqueness: { case_sensitive: false }
   has_secure_password
-  validates :password, presence: true, length: { minimum: 8 }
+  validates :password, presence: true, length: { minimum: 8 }, on: :create
   
   has_many :lyrics
   
