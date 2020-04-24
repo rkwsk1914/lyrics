ee74589ad01149665dfe188153fc2397d0b26726
R.Kawasaki <gettodowithallhaste9@gmail.com>
2020-04-24 16:03:09 +0000

    fix navbar and form, Add jQuery to fadeout alert

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ApplicationRecord
                     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
                     uniqueness: { case_sensitive: false }
   has_secure_password
+  validates :password, presence: true, length: { minimum: 8 }
+  
   has_many :lyrics
   
   has_many :favorites
